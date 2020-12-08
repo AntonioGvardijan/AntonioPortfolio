c0107b6eda3c60ff0d305111b32b92cd043f6941
unknown <gvardijan.a@gmail.com>
2020-12-08 21:44:02 +0100

    need-align-fix

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import logo from '../Images/Logo/Logo2.png';
 
 /*flex flex-column flex-wrap*/
+/*<h4 className="w-full max-w-screen-sm mx-4 my-12">I'm a web developer and designer based in Zagreb, Croatia specializing in building and designing 
+            web pages, designing promotional material and creating 3D videos.
+        </h4>*/
 
 function Main(){
     return(
-    <div className="w-full flex flex-column flex-wrap justify-center relative h-screen">
-        <div className="w-full my-12">
-            <img src={logo} className="w-4/5 max-w-screen-sm"></img>
+    <div className="w-full flex flex-column flex-wrap relative justify-center h-screen">
+        <div className="w-full py-12">
+            <img src={logo} className="block mx-auto max-w-screen-sm"></img>
         </div>
         
-            <h4 className="w-full max-w-screen-sm mx-4 my-12">I'm a web developer and designer based in Zagreb, Croatia specializing in building and designing 
-                web pages, designing promotional material and creating 3D videos.
-            </h4>
+        <p className="w-full text-white max-w-screen-sm mx-4 my-12">I'm a web developer and designer based in Zagreb, Croatia specializing in building and designing 
+            web pages, designing promotional material and creating 3D videos.
+        </p>
+
+        <div className="w-full"></div>
         
         
         <div className="indicator">
